e81a1cbf13fe3382fbe7fc28ad23a185980b31fc
Richard Khillah <rkhillah@gmail.com>
2023-11-08 19:09:23 -0800

    Implmement #45-getRoom and partial #45-createRoom
    Room class skeleton has been created. getRoom currently searches database
    for a single room belonging to an admin. If more than one room for an
    admin is found, an error is thrown.
    
    createRoom has been commented out, and needs to be fully fleshed out.
    
    Note: AdminPage.js has been updated with an example of how to use such
    a function.

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,19 +1,34 @@
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
 
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 export class Room {
     constructor(adminId) { 
         this.adminId = adminId;
-        console.log('adminId ' + adminId);
+        console.log('adminId ' + adminId + ": typeof " + typeof adminId);
     }
 
-    createRoom(adminId) {
+    async createRoom(adminId) {
+        // let roomCode = null;
+        // try {
+        //     // TODO: sort this out
+        //     roomCode = await this.getRoom(adminId);
+        //     // await this.getRoom(adminId);
+        //     console.log("createRoom roomCode: " + roomCode);
+
+        // } catch (e) {
+        //     console.log("Error getting document: ", e);
+        // }
         
-        let roomCode = this._generateRoomCode(4)
-        console.log("roomCode " + roomCode);
+        // if ( !roomCode ) {
+        //     roomCode = this._generateRoomCode(4)
+        // }
+        // console.log("roomCode " + roomCode);
 
-        //TODO: Save code to database
+        // //TODO: Save code to database
 
-        return roomCode;
+        // return roomCode;
     }
 
     _generateRoomCode(length) {
@@ -30,9 +45,21 @@ export class Room {
         // remove all players from room
     }
 
-    getRoom(adminId) {
-        // TODO: query database for room belonging to admin
+    async getRoom(adminId) {
+        const q = query(collection(db, "rooms"), where("adminId", "==", adminId));
         
+        const querySnapshot = await getDocs(q);
+
+        // Currently, support admin having only one room.
+        if (querySnapshot.size > 1) {
+            throw "More than one roome exists for this admin";
+        } else if (querySnapshot.empty) {
+            throw "No room exists for admin";
+        }
+
+        const one_room = querySnapshot.docs.map((doc) => doc.data())[0];
+
+        return one_room.code;
     }
 
     joinRoom(code, playerId) {
