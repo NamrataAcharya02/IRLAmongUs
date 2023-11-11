d25163b2817921ddf1e431e48ca3b6c9046f10e2
Hongkun Li <hongkun@Hongkuns-Air.lan>
2023-11-10 16:12:27 -0800

    Added Firebase RTDB

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,19 +13,20 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC8_TMAHAg-9Fzq4kIBDzf9_veCPdqUHGY",
+  appId: "1:299437319897:web:1a4aff6578a93b98cd40c8",
   authDomain: "irl-among-us-d5453.firebaseapp.com",
+  databaseURL: "https://irl-among-us-d5453-default-rtdb.firebaseio.com",
+  measurementId: "G-1SS9R7WKDG",
+  messagingSenderId: "299437319897",
   projectId: "irl-among-us-d5453",
   storageBucket: "irl-among-us-d5453.appspot.com",
-  messagingSenderId: "299437319897",
-  appId: "1:299437319897:web:1a4aff6578a93b98cd40c8",
-  measurementId: "G-1SS9R7WKDG"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // firebase objects
-export const googleAuthProvider = new GoogleAuthProvider();
-
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleAuthProvider = new GoogleAuthProvider();
+export const rtdb = getDatabase(app);
