89a3bfd30dee9efce6ba14624de439d28cf8a437
Richard Khillah <rkhillah@gmail.com>
2023-11-28 02:24:29 -0800

    add comments
    Note: a couple scaffold changes were made across each Controller.

--- src/controllers/PlayerRoomController.js
+++ src/controllers/PlayerRoomController.js
@@ -1,17 +1,52 @@
 import RoomController from "./RoomController";
 
 export default class PlayerRoomController extends RoomController {
-    player;
+    player; // Player object
 
     constructor() {
         super();
     }
 
-    joinRoom() {}
-    leaveRoom() {}
-    callMeeting() {}
-    markSelfDead() {}
-    markTaskComplete(taskId) {}
-    castVote(playerId) {}
-    getVisibleTasks() {}
+    joinRoom(roomCode) {
+        // set user room to roomCode
+        // set room from the return of calling Room.joinRoom
+    }
+
+    leaveRoom() {
+        // remove player id from room.playerids
+        // delete player db instance
+    }
+
+    callMeeting() {
+        // TODO: SOrt this out
+        // update room status
+        // create emergency room db instance with same roomCode as room
+        // OR create room.createEmergencyField() that creates
+        // a db instance to keep track of voting
+        // OR create a subcollection of room for meetings, to keep track of all 
+        // meeting results.
+    }
+
+    markSelfDead() {
+        // if player is crewmate,
+        // then update player status
+    }
+
+    markTaskComplete(taskId) {
+        // update player tasklists task to completed
+    }
+
+    castVote(playerId) {
+        // If the room status is emergency meeting
+        // add a vote to the emergency room's votes list
+        // after casting a vote, ...?
+    }
+
+    getVisibleTasks() {
+        // Display up to four tasks that have not been completed.
+    }
+
+    getTasklistStatus() {
+        // return (number of tasks complete, number of tasks requred)
+    }
 }
--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,17 +1,30 @@
 import { Room } from "../models/Room";
 
 export default class RoomController {
-    room;
-    listener; // "callback" function
-    players;
-    constructor() {
+    room;   // Room object
+    user;   // TODO: User object (user would be super to Admin and Player)
+    observerCallback;   // "callback" function
+    playerIdNameList;    // list of Player objects formed from room
+    constructor() {}
 
+    notifyObserver() {
+        // run observerCallback
     }
 
-    notify() {}
-    addListener(listener) {}
-    joinRoom() {}
-    getActivePlayers() {}
-    callMeeting() {}
-    viewRole() {}
+    addObserverCallback(observerCallback) {
+        // add observer to Room object
+        // add observer to User object
+    }
+
+    getActivePlayers() {
+        // return a list tuples (playerId, playerName) from playerIdNameList
+    }
+
+    callMeeting() {
+        // Update room status
+    }
+
+    viewRole() {
+        // View the Users role (admin, crewmate, imposter)
+    }
 }
