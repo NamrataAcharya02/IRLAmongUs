3e25ec2b57b5f1a16a7f79297b416f72a4fa0de5
bluepaw59 <96216128+bluepaw59@users.noreply.github.com>
2023-11-27 16:16:33 -0800

    cleaned up unused code

--- src/pages/PlayerGame.js
+++ src/pages/PlayerGame.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import { useSelector } from 'react-redux'
 import background from "../images/stars-background.jpg";
-import FrontendTaskList from "../development-components/DevTaskListComponent.js";
 
 
 function PlayerGame(){
