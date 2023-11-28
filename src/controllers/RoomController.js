38e0f14da5c0c929121530551684a0d714f70722
Richard Khillah <rkhillah@gmail.com>
2023-11-28 00:54:10 -0800

    add RoomController scaffold

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,7 +1,17 @@
 import { Room } from "../models/Room";
 
 export default class RoomController {
+    room;
+    listener; // "callback" function
+    players;
     constructor() {
-        
+
     }
+
+    notify() {}
+    addListener(listener) {}
+    joinRoom() {}
+    getActivePlayers() {}
+    callMeeting() {}
+    viewRole() {}
 }
