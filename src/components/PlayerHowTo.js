838bb55e78a4328b826fcea3e3169064308fa509
pinlunix <pheonlu@gmail.com>
2023-12-03 14:39:11 -0800

    Made How To components for Admin and Player "How to Play"

---
+++ src/components/PlayerHowTo.js
@@ -0,0 +1,89 @@
+import React, {useState} from "react";
+
+function PlayerHowTo() {
+    const [isHelpScreen, setHelpScreen] = useState(false); //how to play overlay
+
+    //function for toggling emergency screen to use (can also directly set emergency screen)
+    const toggleHelpScreen = () => {
+    setHelpScreen(!isHelpScreen);
+    };
+
+    return (
+        <div>
+            {isHelpScreen && (
+                <div className="overlay-help">
+                    <h1>HOW TO PLAY</h1>
+                    <div className="center">
+                        <div className="help-in-box">
+                            {/* Close "How to Play" pop-up */}
+                            <button className="back" onClick={toggleHelpScreen}>Close</button>
+                            {/* Crewmate How to Play */}
+                            <h2 className="whiteh2">Crewmate</h2>
+                            <ol className="helpList">
+                                <li>
+                                    To win: Complete all tasks before Imposters kill all 
+                                    Crewmates to win or vote out all Imposters!
+                                </li>
+                                <li>
+                                    Report “dead” bodies, which calls an emergency meeting. 
+                                    You also have the option to not report a dead body if you see one.
+                                </li>
+                                <li>
+                                    Crewmates can ask the Admin to call an emergency meeting.
+                                </li>
+                                <li>
+                                    In the emergency meeting, vote who you think is the Imposter. 
+                                    You can elect to “Skip” vote. If “Skip” vote has the most votes, 
+                                    no one will be voted out.
+                                </li>
+                                <li>
+                                    If you are voted out in an emergency meeting, you are dead.
+                                </li>
+                                <li>
+                                    If you are dead, you can not communicate. You should still attend emergency meetings.
+                                </li>
+                                <li>
+                                    Dead Crewmates should mark themselves as dead by clicking “Mark Self Dead”.
+                                </li>
+                            </ol>
+                        </div>
+                        {/* Imposter How to Play */}
+                        <div className="help-in-box">
+                            <h2 className="whiteh2">Imposter</h2>
+                            <ol className="helpList">
+                                <li>
+                                    To win: Kill crewmates (method to “kill” a player decided before the game starts 
+                                    or vote them out in emergency meetings) until the number of Imposters alive is 
+                                    the same as the number of Crewmates alive!
+                                </li>
+                                <li>
+                                    Your tasks are fake. Blend in with the Crewmates by pretending to do tasks.
+                                </li>
+                                <li>
+                                    You can either report a dead body or run away to escape suspicion.
+                                </li>
+                                <li>
+                                    Imposters can ask the Admin to call an emergency meeting 
+                                    (This is one way to bluff innocence).
+                                </li>
+                                <li>
+                                    In emergency meetings, avoid suspicion by bluffing or 
+                                    throwing suspicion on other Crewmates.
+                                </li>
+                                <li>
+                                    If you are voted out in an emergency meeting, you are dead.
+                                </li>
+                                <li>
+                                    Don’t get caught!
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            )}
+            <button className="helpButton" onClick={toggleHelpScreen}>How to Play</button>
+        </div>
+    );
+}
+
+export default PlayerHowTo
--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -8,7 +8,7 @@ import { Task, TaskList } from "../models/TaskList.js";
 import background from "../images/stars-background.jpg";
 import { useNavigate } from 'react-router-dom';
 import {auth} from "../firebase.js";
-import Collapsible from "react-collapsible";
+import AdminHowTo from "../components/AdminHowTo.js";
 
 let room = null;
 
@@ -16,14 +16,8 @@ const AdminPage = () => {
   const [data, setData] = useState(0);
   const [tasklist, setTasklist] = useState([]) //object that stores tasklist
   const [listName, setListName] = useState("") //name of tasklist
-  const [isHelpScreen, setHelpScreen] = useState(false); //how to play overlay
   const navigate = useNavigate();
   console.log(tasklist)
-  
-  //function for toggling emergency screen to use (can also directly set emergency screen)
-  const toggleHelpScreen = () => {
-    setHelpScreen(!isHelpScreen);
-  };
 
   //don't exceed number of tasks
   const hasEmptyTask = tasklist.some((singleTask, index) => {
@@ -151,125 +145,7 @@ const AdminPage = () => {
         <button className="back">Back</button>
       </Link>
       {/* "How to Play" pop-up overlay for Players */}
-      {isHelpScreen && (
-                <div className="overlay-help">
-                    <h1>HOW TO PLAY</h1>
-                    <div className="center">
-                        <div className="help-in-box">
-                            {/* Close "How to Play" pop-up */}
-                            <button className="back" onClick={toggleHelpScreen}>Close</button>
-                            {/* Crewmate How to Play */}
-                            <h2 className="whiteh2">Admin</h2>
-                            <ol className="helpList">
-                                <li>
-                                    Create tasks!
-                                </li>
-                                <li>
-                                    Setting up Gameplay!
-                                </li>
-                                <li>
-                                    Facilitate Gameplay!
-                                </li>
-                            </ol>
-                            {/* Collapsible Sections for How to Play */}
-                            {/* Gameplay Loop Section */}
-                            <Collapsible trigger="How the game works">
-                              <ol className="helpList">
-                                <li>
-                                  To start your own game of IRL Among Us, start by creating a task list. 
-                                  Refer to the "Tasks" section on how to create tasks.
-                                </li>
-                                <li>
-                                  Refer to the "Setting Up Gameplay" section for gameplay settings and preparing
-                                  players for how to play the game.
-                                </li>
-                                <li>
-                                  Then, click “Start a Room”.
-                                </li>
-                                <li>
-                                  You will be met with a page that displays a room code. 
-                                  Players can join your room by entering the Room Code in “Join a Room”.
-                                </li>
-                                <li>
-                                  Once all players have joined, start the game.
-                                </li>
-                                <li>
-                                  During the game, players may ask the Admin (you) to start an emergency meeting. 
-                                  You can call an emergency meeting by clicking the “Emergency Meeting” button.
-                                </li>
-                                <li>
-                                  Players discuss their findings and who they believe is the Imposter in emergency meetings. 
-                                  The meeting ends after all players cast their votes on who they think is the Imposter.
-                                </li>
-                                <li>
-                                  Killed players should attend emergency meetings but they cannot communicate.
-                                </li>
-                                <li>
-                                  You may end the game at any time for any reason by clicking the “End Game” button.
-                                </li>
-                                <li>
-                                  The game ends when one of these win conditions are satisfied:
-                                  <ul>
-                                    <li>The task target completion goal has been met (Crewmate win).</li>
-                                    <li>All Imposters have been voted out (Crewmate win).</li>
-                                    <li>The number of Imposters alive is the same as the number of Crewmates alive (Imposter win).</li>
-                                  </ul>
-                                </li>
-                              </ol>
-                            </Collapsible>
-                            {/* Tasks Section */}
-                            <Collapsible trigger="Creating Tasks">
-                              <ol className="helpList">
-                                <li>
-                                  You will be making a list of tasks for your players to complete! 
-                                  The tasks will be done in real life by your players.
-                                </li>
-                                <li>
-                                  When you start a game, (not to be confused with start a room,) 
-                                  the tasks you create will be randomized and given out to your players.
-                                </li>
-                                <li>
-                                  Setting up Tasks: Create 4 difficult tasks and 10 easy tasks.
-                                </li>
-                                <li>
-                                  Task examples:
-                                  <ul>
-                                    <li>The task target completion goal has been met (Crewmate win).</li>
-                                    <li>All Imposters have been voted out (Crewmate win).</li>
-                                    <li>The number of Imposters alive is the same as the number of Crewmates alive (Imposter win).</li>
-                                  </ul>
-                                </li>
-                              </ol>
-                            </Collapsible>
-                            {/* Setting Up Gameplay Section */}
-                            <Collapsible trigger="Setting Up Gameplay">
-                              <ol className="helpList">
-                                <li>
-                                  Depending on the number of players, the number of Imposters should be adjusted. 
-                                  A good guideline is for every 4 players, add one Imposter. 
-                                  For example: if you have 10 players, have 2 Imposters. 
-                                  If you have 5 players, have 1 Imposter.
-                                </li>
-                                <li>
-                                  Teach your players how imposters will “kill” crewmates. 
-                                  Some examples include a double tap on a shoulder, a tap with a pencil, or a wink.
-                                </li>
-                                <li>
-                                  Teach your players how to act “dead”. Some examples include sitting down on the spot, 
-                                  cracking open a glow stick, or raising a hand. 
-                                  Don’t forget to remind them that they should mark themselves as “dead”!
-                                </li>
-                                <li>
-                                  At the beginning of a game, it is recommended for all players to close their eyes, 
-                                  and then have all the Imposters open their eyes to see who the other Imposters are.
-                                </li>
-                              </ol>
-                            </Collapsible>
-                        </div>
-                    </div>
-                </div>
-            )}
-            <button className="helpButton" onClick={toggleHelpScreen}>How to Play</button>
+      <AdminHowTo></AdminHowTo>
 
       {/* TaskList:: */}
       <div className="text-in-box">
