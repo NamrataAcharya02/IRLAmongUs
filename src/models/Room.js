5fa391465072b6ce312e5602437db5a25c33e11b
Richard Khillah <rkhillah@gmail.com>
2023-12-02 17:21:03 -0800

    fix bug in roomConverter (#87)

--- src/models/Room.js
+++ src/models/Room.js
@@ -348,6 +348,7 @@ const roomConverter = {
             adminId: room.getAdminId(),
             code: room.getRoomCode(),
             createdAt: serverTimestamp(),
+            tasklist: room.getTaskList(),
             numImposters: room.getNumImposters(),
             numTasksToDo: room.getNumTasksToDo(),
             playerIds: room.getPlayerIds(),
