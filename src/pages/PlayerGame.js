cb0420256b1ed8f42d07c63f18ae96cceec41ce5
bluepaw59 <96216128+bluepaw59@users.noreply.github.com>
2023-12-04 05:55:41 -0800

    connected up task list to controller
    modified player game screens to listen off gameState variable

--- src/pages/PlayerGame.js
+++ src/pages/PlayerGame.js
@@ -1,49 +1,51 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Link} from "react-router-dom";
 import { useSelector } from 'react-redux'
 import background from "../images/stars-background.jpg";
-import PlayerTaskList from "../components/PlayerTaskList";
 import VoteScreen from "../components/VoteScreen";
 import useSound from 'use-sound';
 import meetingSound from '../sounds/meetingSFX.mp3'
 import meetingBackground from "../images/stars-background.jpg";
 import { useNavigate } from "react-router-dom";
+import PlayerGameController from "../controllers/PlayerGameController";
+import { auth } from "../firebase.js";
 
 function PlayerGame(){
-    const [isEmergencyScreen, setEmergencyScreen] = useState(false);
-    const [isMeetingRoom, setMeetingRoom] = useState(false);
     const [currentComplete, setComplete] = useState(0);
     const [toComplete, setToComplete] = useState(20);
     const [playSound] = useSound(meetingSound);
-    const [gameEnd, setGameEnd] = useState();
+    const [gameState, setGameState] = useState();
+    const [tasks, setTasks] = useState([]);
+
     const navigate = useNavigate();
+    let playerId = auth.currentUser.uid; // dummy for testing
+    const forceUpdate = React.useReducer(() => ({}))[1];
+    let controller = useRef(new PlayerGameController(playerId, forceUpdate));
 
     const setImposterWin = () => {
-        setGameEnd("imposterWin");
+        setGameState("imposterWin");
     }
 
     const setCrewmateWin = () => {
-        setGameEnd("crewmateWin");
+        setGameState("crewmateWin");
+    }
+
+    const setEmergencyScreen = () => {
+        playSound();
+        setGameState("emergency");
+    }
+
+    const setVotingScreen = () => {
+        setGameState("voting")
+    }
+
+    const setInGame = () => {
+        setGameState("");
     }
 
     const returnHome = () => {
         navigate("/");
     }
-
-    //function for toggling emergency screen to use (can also directly set emergency screen)
-    const toggleEmergencyScreen = () => {
-        setEmergencyScreen(!isEmergencyScreen);
-        playSound();
-    };
-    //function for toggling emergency meeting room screen
-    const toggleMeetingRoom = () => {
-        setMeetingRoom(!isMeetingRoom);
-    };
-    //function to toggle both emergency meeting room and emergency screen
-    function leaveMeetingRoom() {
-        setMeetingRoom(!isMeetingRoom);
-        setEmergencyScreen(!isEmergencyScreen);
-    };
 
     //function to add 1 to currentComplete (tracks number of completed tasks)
     function completeATask(){
@@ -54,6 +56,28 @@ function PlayerGame(){
         setToComplete(numCrewmates * numTasksPerCrewmate);
     }
 
+    const markComplete = (name) => {
+        controller.current.markTaskComplete(name);
+        const updatedTasks = controller.current.getVisibleTasks();
+        setTasks(updatedTasks);
+    }
+
+    function markSelfDead(){
+        controller.current.markSelfDead();
+    }
+
+    useEffect(() => {
+        (async function () {
+            await controller.current.init();
+    
+            console.log(`controller.current.room.getRoomCode(): ${controller.current.room.getRoomCode()}`);
+            console.log(`controller.current.room.getTaskList(): ${controller.current.getVisibleTasks()}`);
+            
+            setTasks(controller.current.getVisibleTasks());
+
+            })();
+      }, []);
+
     return (
         <div className="center" style={{paddingTop: "20px", paddingBottom:"20px", backgroundImage: `url(${meetingBackground})`}}>
             {/* Progress bar shows how many tasks completed (currentComplete) out of total tasks (toComplete) */}
@@ -62,28 +86,37 @@ function PlayerGame(){
             <button onClick={completeATask}>Complete a Task</button>
             {/* <button onClick={playSound}>Beep</button> */}
             {/* Player's task list */}
-            <PlayerTaskList></PlayerTaskList>
+            <ul className="centered-lists">
+		        {tasks.map((task) => (
+                    <div className="task">
+                        <h3>
+                            <input 
+                                type="checkbox"
+                                className="checkbox"
+                                onClick={() => markComplete(task)}
+                                checked={false}
+                            /> 
+                            {task}
+                        </h3>
+                    </div>
+                ))}
+            </ul>
+            <button onClick={markSelfDead}>Mark Self Dead</button>
 
             {/* emergency meeting called screen */}
-            {isEmergencyScreen && (
+            {gameState==="emergency" && (
                 <div className="overlay">
                     <h1>EMERGENCY MEETING CALLED</h1>
-                    <div className="center">
-                        {/* emergency meeting room screen */}
-                        {isMeetingRoom && (
-                            <div className="overlay-meeting" style={{ backgroundImage: `url(${meetingBackground})` }}  >
-                                <h1>Who is the imposter?</h1>
-                                <VoteScreen></VoteScreen>
-                                <div className="center">
-                                    <button onClick={leaveMeetingRoom}>Leave Meeting Room</button>
-                                </div>
-                            </div>
-                        )}
-                        <button onClick={toggleMeetingRoom}>Enter Meeting Room</button>
-                    </div>
+                    <button onClick={setInGame}>Return to Normal</button>
                 </div>
             )}
-            {gameEnd==="imposterWin" && (
+            {gameState==="voting" && (
+                <div className="overlay-meeting" style={{ backgroundImage: `url(${meetingBackground})` }}  >
+                    <h1>Voting in Session</h1>
+                    <button onClick={setInGame}>Return to Normal</button>
+                </div>
+            )}
+            {gameState==="imposterWin" && (
                 <div className="imposter-win">
                     <h1>IMPOSTER VICTORY</h1>
                     <div className="center">
@@ -91,7 +124,7 @@ function PlayerGame(){
                     </div>                   
                 </div>
             )}
-            {gameEnd==="crewmateWin" && (
+            {gameState==="crewmateWin" && (
                 <div className="crewmate-win">
                     <h1>CREWMATE VICTORY</h1>
                     <div className="center">
@@ -99,7 +132,8 @@ function PlayerGame(){
                     </div>
                 </div>
             )}
-            <button onClick={toggleEmergencyScreen}>Call Emergency Meeting</button>
+            <button onClick={setEmergencyScreen}>Call Emergency Meeting</button>
+            <button onClick={setVotingScreen}>Set Voting Screen</button>
             <button onClick={setImposterWin}>Set Imposter Victory</button>
             <button onClick={setCrewmateWin}>Set Crewmate Victory</button>
         </div>
